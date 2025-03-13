ff918d5b2f174e6f608d7e0452844785caaa790d
Mayuresh Chandavarkar <mayureshchandavarkar@Mayureshs-Macbook.local>
2025-03-13 16:33:45 +0530

    Hello.cpp changed

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout<<"Hi!!\n";
-  cout<<"Hello!!\n";
+  cout<<"Hello guys!!\n";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
